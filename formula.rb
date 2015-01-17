1cd852bec3c68e6ab2b2b0e41e5a4c8ed2da8db0
willnet <netwillnet@gmail.com>
2015-01-17 13:55:34 +0900

    Add Formula.execute

--- formula.rb
+++ formula.rb
@@ -1,16 +1,36 @@
+# coding: utf-8
 class Formula
   #|  ビット or
   #&  ビット and
   #+  加算
   #*  乗算
   def self.execute(input)
-    array1 = input.split('*')
-    array2 = array1[1].split('+')
-    array3 = array2[1].split('&')
-    output = eval(array3[1])
-    output = eval(output.to_s + '&' + array3[0])
-    output = eval(output.to_s + '+' + array2[0])
-    output = eval(output.to_s + '*' + array1[0])
+    multiplied = multiply(input)
+    output = multiplied.map do |m| # ['4', '5+6&7|8']
+      plused = plus(m)
+      plused.map do |pl| # ['5', '6&7|8']
+        anded = do_and(pl)
+        anded.map do |a|
+          eval(a)
+        end.map(&:to_i).inject(:&)
+      end.map(&:to_i).inject(:+)
+    end.map(&:to_i).inject(&:*)
     output.to_s
+  end
+
+  def self.multiply(input)
+    input.split('*')
+  end
+
+  def self.plus(input)
+    input.split('+')
+  end
+
+  def self.do_and(input)
+    input.split('&')
+  end
+
+  def self.do_or(input)
+    input.split('|')
   end
 end
