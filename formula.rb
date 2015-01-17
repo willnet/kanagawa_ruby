fed954c8a0e9ced415c04ecef944c3be3e12d959
willnet <netwillnet@gmail.com>
2015-01-17 14:04:54 +0900

    Refactor with block

--- formula.rb
+++ formula.rb
@@ -5,28 +5,30 @@ class Formula
   #+  加算
   #*  乗算
   def self.execute(input)
-    multiplied = multiply(input)
-    output = multiplied.map do |m|
-      plused = plus(m)
-      plused.map do |pl|
-        anded = do_and(pl)
-        anded.map do |a|
+    multiply(input) do |m|
+      plus(m) do |pl|
+        do_and(pl) do |a|
           eval(a).to_i
-        end.inject(:&)
-      end.inject(:+)
-    end.inject(&:*)
-    output.to_s
+        end
+      end
+    end.to_s
   end
 
   def self.multiply(input)
-    input.split('*')
+    input.split('*').map do |m|
+      yield m
+    end.inject(&:*)
   end
 
   def self.plus(input)
-    input.split('+')
+    input.split('+').map do |p|
+      yield p
+    end.inject(&:+)
   end
 
   def self.do_and(input)
-    input.split('&')
+    input.split('&').map do |a|
+      yield a
+    end.inject(&:&)
   end
 end
